af3391edbc9e6880c2264fa40825739bbaaddd05
tarineeprasad <tarineeprasad1066@gmail.com>
2021-07-03 00:19:04 +0530

    added login with session cookies

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,7 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path, notice: "Sucessfully created account"
     else
       render :new
