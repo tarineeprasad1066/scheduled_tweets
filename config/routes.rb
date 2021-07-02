3b0e25445f6365bdd43d61645763e7906b09ddc9
tarineeprasad <tarineeprasad1066@gmail.com>
2021-07-03 00:43:01 +0530

    Added funtionality of logout button

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get "sign_up" , to: "registrations#new"
   post "sign_up" , to: "registrations#create"
+  delete "logout" , to: "sessions#destroy"
 
   root to:"main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
