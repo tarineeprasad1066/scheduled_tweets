7750b2c336349e653ada0f2c726bb10ee62fa71b
tarineeprasad <tarineeprasad1066@gmail.com>
2021-07-22 00:05:45 +0530

    added twitter account model

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,17 @@
 class OmniauthCallbacksController < ApplicationController
   def twitter
-    render plain: "Success!"
+     Current.user.twitter_accounts.create(
+      name: auth.info.name,
+      username: auth.info.nickname,
+      image: auth.info.image,
+      token: auth.credentials.token,
+      secret: auth.credentials.secret,
+    )
+
+    redirect_to root_path, notice: "Sucessfully connected your account"
+  end
+  
+  def auth
+    request.env['omniauth.auth']
   end
 end
